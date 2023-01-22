f132657e004d5ead4aea7d3b943fb7aa6b035a61
최윤석 <cys@choeyunseog-ui-MacBookAir.local>
2023-01-23 04:37:10 +0900

    회원가입 페이지 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import {Routes, Route} from "react-router-dom"
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
-  return (
-      <React.Fragment>
-        <Header/>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-        </Routes>
-      </React.Fragment>
-  )
+    return (
+        <React.Fragment>
+            <Header/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/sign-up" element={<SignUp/>}/>
+            </Routes>
+        </React.Fragment>
+    )
 }
 export default App
